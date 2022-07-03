aace91cf713e845d3a24e5aab32ddcfb52d6fdc6
goodylove474 <goodyc474@gmail.com>
2022-07-03 14:22:48 +0100

    functionalities

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const word = document.querySelector(".word");
 const details = document.querySelector(".detials");
 const content = document.querySelector(".content");
 const wordMeaning = document.querySelector(".meaning");
-const synonmys = document.querySelector(".synonmys .list");
-// const list = document.querySelector(".list");
+const synonmys = document.querySelector(".synonmys");
+const volumePlay = document.querySelector(".play-audio");
+let audio;
 const wordExample = document.querySelector(".example");
 
 // data function
@@ -18,22 +19,21 @@ function data(result, word) {
     infoText.innerHTML = `can't find the meaning of "${word}" please try to search for anotehr word.`;
   } else {
     console.log(result);
+
     wrapper.classList.add("active");
+
     let definitions = result[0].meanings[0].definitions[2];
+
     document.querySelector(".word-p").innerHTML = result[0].word;
+
     document.querySelector(".phonetic").innerHTML = result[0].phonetic;
+
     wordMeaning.innerHTML = definitions.definition;
+
     wordExample.innerHTML = definitions.example;
-    if (definitions.synonmys[0] == undefined) {
-      synonmys.parentElement.style.display = "none";
-    } else {
-      synonmys.parentElement.style.display = "block";
-      synonmys.innerHTML = "";
-      for (let i = 0; i < 5; i++) {
-        let tag = `<span>${definitions.synonmys[i]},</span>`;
-        synonmys.insertAdjacentHTML("beforeend", tag);
-      }
-    }
+    document.querySelector(".partOf").innerHTML =
+      result[0].meanings[0].partOfSpeech;
+    audio = new Audio("https:" + result[0].phonetics[0].audio);
   }
 }
 
@@ -54,4 +54,7 @@ searchInput.addEventListener("keyup", (e) => {
   if (e.key == "Enter" && e.target.value) {
     fetchApi(e.target.value);
   }
+});
+volumePlay.addEventListener("click", () => {
+  audio.play();
 });
