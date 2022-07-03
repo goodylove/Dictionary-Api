6157c13ce23edd06f231f03f5ac59b3579531292
goodylove474 <goodyc474@gmail.com>
2022-07-03 13:08:22 +0100

    functionalities

--- app.js
+++ app.js
@@ -8,16 +8,32 @@ const word = document.querySelector(".word");
 const details = document.querySelector(".detials");
 const content = document.querySelector(".content");
 const wordMeaning = document.querySelector(".meaning");
-const syn = document.querySelector(".synonmys");
-const list = document.querySelector(".list");
-const wordExample = document.querySelector(".meaning");
+const synonmys = document.querySelector(".synonmys .list");
+// const list = document.querySelector(".list");
+const wordExample = document.querySelector(".example");
 
 // data function
 function data(result, word) {
   if (result.title) {
-    infoText.innerHTML = `can't find the meaning of  "${word}" please try to search for anotehr word.`;
+    infoText.innerHTML = `can't find the meaning of "${word}" please try to search for anotehr word.`;
   } else {
     console.log(result);
+    wrapper.classList.add("active");
+    let definitions = result[0].meanings[0].definitions[2];
+    document.querySelector(".word-p").innerHTML = result[0].word;
+    document.querySelector(".phonetic").innerHTML = result[0].phonetic;
+    wordMeaning.innerHTML = definitions.definition;
+    wordExample.innerHTML = definitions.example;
+    if (definitions.synonmys[0] == undefined) {
+      synonmys.parentElement.style.display = "none";
+    } else {
+      synonmys.parentElement.style.display = "block";
+      synonmys.innerHTML = "";
+      for (let i = 0; i < 5; i++) {
+        let tag = `<span>${definitions.synonmys[i]},</span>`;
+        synonmys.insertAdjacentHTML("beforeend", tag);
+      }
+    }
   }
 }
 
